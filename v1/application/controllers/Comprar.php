9ba9772972cdabf5451357b0683e108117e172b5
josue <josue.barrios@gmail.com>
2015-11-26 10:07:11 -0500

    gestion de compra

--- v1/application/controllers/Comprar.php
+++ v1/application/controllers/Comprar.php
@@ -10,16 +10,53 @@ class Comprar extends CI_Controller {
 		parent::__construct();
        	$this->load->database();
        	$this->load->helper('url');
+       	$this->load->helper('form');
        	$this->session;
     }
 	
 	public function resumen($idZapato){
-		$query = "select a.descripcion, a.fabrica, a.linea, a.zuela, a.capellada, a.adorno, a.color, b.nombre as almacen from zapato a, almacen b where a.almacen = b.id";
+		$query = $this->db->query("select a.descripcion, a.fabrica, a.linea, a.zuela, a.capellada, a.adorno, a.color, b.nombre as almacen from zapato a, almacen b where a.almacen = b.id and a.id = ".$this->db->escape($idZapato));
 		$fila = $query->row_array();
 
 		$this->load->view('plantillas/encabezado',$this->session->has_userdata('nombre'));
 		$this->load->view('resumen');
 		$this->load->view('plantillas/pie');
+	}
+
+	public function resumenout($idZapato){
+		$query = $this->db->query("select descripcion, render, precio from zapato where id = ".$this->db->escape($idZapato));
+		$datos = $query->row_array();
+
+		if (count($datos)> 0){
+			$data = array(
+				"descripcion" => $datos["descripcion"],
+				"render" => $datos["render"],
+				"precio" => $datos["precio"]
+				);
+			$this->load->view('plantillas/encabezado',$this->session->has_userdata('nombre'));
+			$this->load->view('resumen', $datos);
+			$this->load->view('plantillas/pie');
+		}
+	}
+
+	public function cerrarCompra(){
+		/*
+		Registrar el almacen si no existe
+		*/
+		$nombre_almacen = $this->input->post('txtAlmacen');
+		$correo = $this->input->post("txtCorreo");
+		$cantidad = $this->input->post("txtCantidad");
+		$precio = $this->input->post("txtPrecio");
+
+		$query = $this->db->query("select id from almacen where correo = '".$this->db->escape($correo)."'");
+		$res1 = $query->row_array();
+		if (count($res1)>0){
+			// registrar el pedido para el almacen
+			
+		}else{
+			// insertar almacen nuevo
+		}
+
 	}
 
 	
--- v1/application/views/resumen.php
+++ v1/application/views/resumen.php
@@ -1,3 +1,63 @@
+
 <div class="row">
-	
+	<h3>Resumen del pedido</h3>
 </div>
+<div class="row">
+	<div class="col-xs-6">
+		<div class="row">
+			<img src="<?php echo base_url($render) ?>" alt="<?php echo $descripcion ?>">	
+		</div>
+		<div class="row">
+			<div class="col-xs-6">
+				<h4><?php echo $descripcion ?></h4>
+			</div>
+			<div class="col-xs-6">
+				<h4>
+					Precio: <span class="label label-primary"><?php echo $precio ?></span>
+					<?php echo form_input(array('name' => 'txtPrecio', 'id' => 'txtPrecio', 'class' => 'form-control', 'type' => 'hidden', 'value' => $precio)) ?>
+				</h4>
+			</div>
+		</div>
+		
+	</div>
+	<div class="col-xs-6">
+	<?php echo form_open(base_url('comprar/cerrarCompra')) ?>
+		<div class="form-group">
+			<label for="txtAlmacen">Nombre del almacen</label>
+			<?php echo form_input(array('name' => 'txtAlmacen', 'id' => 'txtAlmacen', 'class' => 'form-control', 'type' => 'text')) ?>
+		</div>
+		<div class="form-group">
+			<label for="txtCorreo">Correo Electronico</label>
+			<?php echo form_input(array('name' => 'txtCorreo', 'id' => 'txtCorreo', 'class' => 'form-control', 'type' => 'text')) ?>
+		</div>
+		<div class="form-group">
+			<label for="txtCantidad">Cantidad</label>
+			<?php echo form_input(array('name' => 'txtCantidad', 'id' => 'txtCantidad', 'class' => 'form-control', 'type' => 'text')) ?>
+		</div>
+		<div class="form-group">
+			<label for="txtTotal">Total:</label>
+			<?php echo form_input(array('name' => 'txtTotal', 'id' => 'txtTotal', 'class' => 'form-control', 'type' => 'text', 'readonly' => 'readonly')) ?>
+		</div>
+		<div class="form-group">
+			<?php echo form_button(
+					array('name' => 'button',
+						  'class' =>'btn btn-success', 
+						  'type' => 'submit', 
+						  'content' => 'Solicitar pedido')) 
+				?>
+		</div>
+	<?php echo form_close(); ?>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+$("#txtCantidad").on('keyup',function(){
+	var precio = parseInt($("#txtPrecio").val());
+	var cantidad = parseInt($(this).val());
+	console.log(precio);
+	console.log(cantidad);
+	$("#txtTotal").val(precio * cantidad);
+});
+
+</script>
